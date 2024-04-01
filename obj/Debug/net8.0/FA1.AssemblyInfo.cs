a4413bc82856622614ea7be7daaa924ebc3e3f78
Henrico <h4z3y.bc@gmail.com>
2024-04-01 07:55:34 +0200

    added the new build for the program

--- obj/Debug/net8.0/FA1.AssemblyInfo.cs
+++ obj/Debug/net8.0/FA1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FA1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c680774fa01b4a6571c9433c5109bdba29bec7fd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05566ed18c68c32d2a6c8844c2a7a0dfc84ba246")]
 [assembly: System.Reflection.AssemblyProductAttribute("FA1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FA1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
